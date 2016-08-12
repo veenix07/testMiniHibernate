159e528d9222b95e16cf2d9e15659963614cc56c
veenix07 <veenix07@yahoo.com>
2016-08-12 09:16:47 +0800

    added sql structure into the project
    Signed-off-by: veenix07 <veenix07@yahoo.com>

---
+++ src/com/vee/onlinetest/script/mysql/table_creation.sql
@@ -0,0 +1,41 @@
+-- categories table
+CREATE TABLE `categories` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(100) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `updated_date` varchar(45) DEFAULT NULL,
+  `created_by` varchar(45) DEFAULT NULL,
+  `updated_by` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`category_id`),
+  UNIQUE KEY `category_name_UNIQUE` (`category_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- questions table
+CREATE TABLE `questions` (
+  `question_id` int(11) NOT NULL,
+  `question_text` varchar(500) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `created_by` varchar(45) DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  `updated_by` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`question_id`),
+  KEY `category_id_idx` (`category_id`),
+  CONSTRAINT `category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- answers table
+CREATE TABLE `answers` (
+  `answer_id` int(11) NOT NULL,
+  `question_id` int(11) DEFAULT NULL,
+  `answer_text` varchar(45) DEFAULT NULL,
+  `answer_bool` tinyint(1) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `created_by` varchar(45) DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  `updated_by` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`answer_id`),
+  KEY `question_id_idx` (`question_id`),
+  CONSTRAINT `question_id` FOREIGN KEY (`question_id`) REFERENCES `questions` (`question_id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
